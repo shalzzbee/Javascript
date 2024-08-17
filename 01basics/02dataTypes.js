6f2d0348e59d47292adffd6560809913e92b98a4
Shalini Budholiya <119559033+shalzzbee@users.noreply.github.com>
2024-08-17 15:57:41 +0000

    Datatypes and Ecmascript

---
+++ 01basics/02dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //new syntax and treat all js code as newer version of js
+//alert(6+3);  //cant be used as we are using node js not browser
+
+
+console.log(3
+    +
+    3)  //code readability should be high  not acceptable
+
+
+// read more on EcmaScript TC39 and mdn
+
+//DataTypes
+
+let name="shalini"  //string
+let age=18          //number 
+let isLoggedIn=false;  //boolean
+
+
+//number => 2 to power 53 (range)
+//bigInt =>large no.s
+//string => " "
+//boolean => true or false
+//null => stand alone value ---> representation of empty value
+//undefined => value not assigned
+//symbol => (react)-->uniqueness
+
+//objects
+
+console.log(typeof "shalini")  //string
+console.log(typeof age)  //number
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
+
+
+

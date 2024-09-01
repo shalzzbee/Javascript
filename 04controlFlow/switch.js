416e4bb006e38092e53cc462fed3bbd0d66e61b4
Shalini Budholiya <119559033+shalzzbee@users.noreply.github.com>
2024-09-01 15:37:26 +0000

    Swtich case

---
+++ 04controlFlow/switch.js
@@ -0,0 +1,34 @@
+//switch case
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+//duplicate = shift + alt + down arrow
+
+//const month = "3";
+const month = "march";
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("pagal");
+        break;
+}
+
+//if break is not put then all the statements after the true condition will run
+//except the deafult case

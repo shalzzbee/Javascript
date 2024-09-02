b01978a58661dfac173dfb7294292c77625a731f
Shalini Budholiya <119559033+shalzzbee@users.noreply.github.com>
2024-09-02 17:02:51 +0000

    While and Do-while Loop

---
+++ 05iterations/two.js
@@ -0,0 +1,28 @@
+//while
+
+
+let i = 0;
+while (i <= 10) {
+    //console.log(`value of i = ${i}`);
+    i = i + 2;
+}
+
+
+//array
+let arr = ["flash", "superman", "batman"];
+let j = 0;
+while (j < arr.length) {
+    //console.log(`value is ${arr[j]}`);
+    j++;
+}
+
+
+
+//do while loop
+
+let score = 11;
+
+do {
+    console.log(`score is ${score}`);  //1 baar toh print hoga hi
+    score++;
+} while (score < 11);
